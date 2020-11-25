6e1358bb441e8d267687bc8946a1763ad96b31a7
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2020-11-24 19:03:15 -0500

    initial deploy

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Dog.create(name: "Sparky", age: 5, breed: "Bulldog")
+Dog.create(name: "Spot", age: 5, breed: "Dalmation")
+Dog.create(name: "Goldy", age: 5, breed: "Golden Retriever")
